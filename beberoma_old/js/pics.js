90869950fdb5f51711c32ba6a43f19e55cd01036
Caner Taylan <53885402+CanerTaylan@users.noreply.github.com>
2022-10-25 19:32:46 +0300

    Add files via upload

---
+++ beberoma_old/js/pics.js
@@ -0,0 +1,239 @@
+// JavaScript Document
+$(document).ready(function() {
+$('.fancybox').fancybox();
+$("#fancybox-galery").click(function() {
+				$.fancybox.open([
+					{
+				openEffect : 'elastic',
+				openSpeed  : 150,
+
+				closeEffect : 'elastic',
+				closeSpeed  : 150,
+
+				closeClick : true,
+				
+						href : 'pics/acilis/01.jpg',
+						title : 'Açılış öncesi',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}, {
+						href : 'pics/acilis/02.jpg',
+						title : 'Herşey hazır. Konuklarımızı bekliyoruz',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}, {
+						href : 'pics/acilis/03.jpg',
+						title : 'Nilüfer Belediye Başkanımız Sayın Mustafa Bozbey açılışımızı yapıyor.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/04.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/05.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/06.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/07.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/08.jpg',
+						title : 'Kurdelemiz kesilirken',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+						{
+						href : 'pics/acilis/14.jpg',
+						title : 'Bizler ve Mustafa Bozbey',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/09.jpg',
+						title : 'Tüm konuklamıza teşekkür ederiz.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/10.jpg',
+						title : 'Tüm konuklamıza teşekkür ederiz.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/11.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/12.jpg',
+						title : 'Caillou da  bizimleydi.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/13.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}
+				], {
+					helpers : {
+						thumbs : {
+							width: 50,
+							height: 30
+						}
+					}
+				});
+			});
+});
+// JavaScript Document
+$(document).ready(function() {
+$('.fancybox').fancybox();
+$("#fancybox-galery2").click(function() {
+				$.fancybox.open([
+					{
+				openEffect : 'elastic',
+				openSpeed  : 150,
+
+				closeEffect : 'elastic',
+				closeSpeed  : 150,
+
+				closeClick : true,
+				
+						href : 'pics/acilis/01.jpg',
+						title : 'Açılış öncesi',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}, {
+						href : 'pics/acilis/02.jpg',
+						title : 'Herşey hazır. Konuklarımızı bekliyoruz',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}, {
+						href : 'pics/acilis/03.jpg',
+						title : 'Nilüfer Belediye Başkanımız Sayın Mustafa Bozbey açılışımızı yapıyor.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/04.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/05.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/06.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/07.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/08.jpg',
+						title : 'Kurdelemiz kesilirken',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+						{
+						href : 'pics/acilis/14.jpg',
+						title : 'Bizler ve Mustafa Bozbey',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/09.jpg',
+						title : 'Tüm konuklamıza teşekkür ederiz.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/10.jpg',
+						title : 'Tüm konuklamıza teşekkür ederiz.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/11.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/12.jpg',
+						title : 'Caillou da  bizimleydi.',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					},
+					{
+						href : 'pics/acilis/13.jpg',
+						title : '',
+						prevEffect : 'elastic',
+						nextEffect : 'elastic',
+						padding: 0
+					}
+				], {
+					helpers : {
+						thumbs : {
+							width: 50,
+							height: 30
+						}
+					}
+				});
+			});
+});
+

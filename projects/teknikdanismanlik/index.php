4e9da4161215ece55e4fadd355b8666f2df0c689
Caner Taylan <53885402+CanerTaylan@users.noreply.github.com>
2022-10-29 15:33:01 +0300

    Add files via upload

---
+++ projects/teknikdanismanlik/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:::::::: TEKNIK DANISMANLIK - SCHLEIFTECHNIK ::::::::</title>
+<meta name="google-site-verification" content="_jFpEciqKwkSRpq7WHMhRSe_9ukIZ3BTPz0oSMlX8hA" />
+<link rel="shortcut icon" href="pics/drawing.ico"/>
+</head>
+
+<body>
+<table width="800" height="600" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><img src="pics/index.png" width="800" height="600" border="0" usemap="#Map"></td>
+  </tr>
+</table>
+<map name="Map">
+  <area shape="rect" coords="298,387,352,436" href="tr/index.php">
+  <area shape="rect" coords="369,387,422,437" href="de/index.php">
+  <area shape="rect" coords="436,387,494,440" href="eng/index.php">
+</map>
+</body>
+</html>
